4456fbdfc3b4c3c23db5c61d1421202887dc75ee
ALPHA <saikatmahara7895@gmail.com>
2026-02-08 12:32:26 +0530

    Disable DB for Vercel serverless

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy(app)
 import nltk
 from nltk.tokenize import sent_tokenize
 from duckduckgo_search import DDGS
