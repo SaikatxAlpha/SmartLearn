a4c139133111eaea4435515c1102789ef4322def
ALPHA <saikatmahara7895@gmail.com>
2026-02-08 12:17:23 +0530

    Fix static folder and cleanup for Vercel

--- app.py
+++ app.py
@@ -88,9 +88,8 @@ def login():
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    app.run()
+
 
 
 
