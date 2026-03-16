bfc56e80a66cebea7ae764694a1bdc122341858a
ALPHA <saikatmahara7895@gmail.com>
2026-03-16 23:39:19 +0530

    Terms

--- app.py
+++ app.py
@@ -672,6 +672,28 @@ def verify_payment():
 def payment_success():
     return render_template("payment_success.html")
 
+# Privacy terms Routes
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+
+@app.route("/terms")
+def terms():
+    return render_template("terms.html")
+
+@app.route("/disclaimer")
+def disclaimer():
+    return render_template("disclaimer.html")
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
